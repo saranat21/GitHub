ed7f427fed2c751094decc492b0abce72ed7e562
saranat21 <saranmuthuraju@gmail.com>
2021-09-28 21:15:50 +0530

    new project github into git

---
+++ src/com/github/Training/TrainingClass.java
@@ -0,0 +1,17 @@
+package com.github.Training;
+
+public class TrainingClass {
+	
+	public void Saran() {
+		System.out.print("kumar's elder bro is");
+	}
+	public void Prabha() {
+		System.out.println(",Appu.");
+
+}
+public static void main(String[] args) {
+	TrainingClass T = new  TrainingClass();
+	T.Saran();
+	T.Prabha();
+}
+}
